28d7c0ebca862304fa00b209f3ce05527980c962
rifat-sultana <rifatsultana88.web@gmail.com>
2026-06-20 01:07:45 +0600

    Added book.js for book-related operations

--- config/db.js
+++ config/db.js
@@ -3,7 +3,15 @@ const { MongoClient, ServerApiVersion } = require("mongodb");
 let database;
 
 const connectDB = async () => {
-  const client = new MongoClient(process.env.MONGODB_URI, {
+ 
+  const uri = process.env.MONGO_DB_URI;
+  if (!uri) {
+    throw new Error('Please define the MONGO_URI environment variable');
+  } else {
+    console.log('Got values from environment!!')
+  }
+
+  const client = new MongoClient(process.env.MONGO_DB_URI, {
     serverApi: {
       version: ServerApiVersion.v1,
       strict: true,
